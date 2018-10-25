e8a376cbc186e52ca298681cfa741bade159c3d7
Kevin Fronczak <kfronczak@gmail.com>
2018-10-25 16:47:45 -0400

    Add ability to pipe python to syslog

--- app/start.sh
+++ app/start.sh
@@ -2,11 +2,18 @@
 service rsyslog start
 service cron restart
 crontab -r
+
+echo "module(load='imudp')" >> /etc/rsyslog.conf
+echo "input(type='imudp' port='514')" >> /etc/rsyslog.conf
+service rsyslog restart
+
 if [ -e /work/my_requirements.txt ]
 then
     pip install -r /work/my_requirements.txt
 fi
+
 python /app/run.py
 crontab /etc/cron.d/pycron
+
 trap : TERM INT
 tail -f /var/log/syslog & wait
